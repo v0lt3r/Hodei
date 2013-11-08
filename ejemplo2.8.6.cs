0867aa201f43b322e7c1370faffb7183818bb333
volter <volter90@gmail.com>
2013-11-08 18:25:34 +0100

    ejemplo 286 resuelto

---
+++ ejemplo2.8.6.cs
@@ -0,0 +1,28 @@
+using System;
+public class Ejemplo2_8_4
+{
+	public static void Main()
+	{
+		int ancho;
+		int alto;
+		Console.Write("Introduzca el ancho de rectangulo: ");
+		ancho = int.Parse(Console.ReadLine());
+		Console.Write("Introduzca el alto del rectangulo: ");
+		alto = int.Parse(Console.ReadLine());
+		
+		for (int i = 0; i < alto ; i++)
+		{
+			for (int j = 0; j < ancho ; j++)
+			{
+				if( (i==0) || (i==alto-1) || (j==0) || (j==ancho-1) )
+					Console.Write("*");
+				else 
+					Console.Write(" ");
+			}
+			Console.Write("\n");
+		}
+		
+		Console.WriteLine("");
+		Console.WriteLine("Adios.");
+	}
+}

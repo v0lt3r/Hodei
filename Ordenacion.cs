387b577349c3105c2c040d987d80ab5ecf129872
Filippo Didonna <huracanster@gmail.com>
2013-11-21 17:38:13 +0100

    ejercicios varios _ mod ordenacion

--- Ordenacion.cs
+++ Ordenacion.cs
@@ -6,38 +6,41 @@ using System.Threading.Tasks;
 
 namespace ordenacion_de_ints
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] array = new int[10];
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = new int[10];
 
-            for (int i = 0; i < array.Length; i++)
-			{ 
-                Console.WriteLine("escriba el numero {0} a añadir:", i);
-                array[i] = Convert.ToInt16 (Console.ReadLine());
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine("escriba el numero {0} a añadir:", i);
+				array[i] = Convert.ToInt16 (Console.ReadLine());
 			}
 
-            int temp = 0;
+			int temp = 0;
+			int bandera = 0;
+			do
+			{
+				bandera = 0;
+				for (int j = 0; j < array.Length-2; j++)
+				{
+					if (array[j] > array[j+1])
+					{
+						temp = array[j];
+						array[j] = array[j+1];
+						array[j+1] = temp;
+						bandera = 1;
+					}
+				}
+			}
+			while (bandera == 1);
 
-            for (int i=1; i<array.Length-1; i++)
-            {
-                for (int j=i+1; j == array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                       temp = array[j];
-                       array[j] = array[i];
-                       array[i] = temp;
-                    }
-
-
-                }
-            }
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
+				for (int i = 0; i < array.Length; i++)
+			{
+				Console.WriteLine(array[i]);
+			}
+			Console.ReadKey();
         }
     }
 }

4301397b3d9e73c8108ec243da2376da6d6dabac
Filippo Didonna <huracanster@gmail.com>
2013-12-06 22:15:45 +0100

    Visual Studio

---
+++ visual_studio/cielo estrellado/cielo estrellado/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cielo_estrellado
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const int longitud = 20;
+			const int largo = 70;
+			char[, ] estrellas = new char[longitud, largo];
+
+			Random generador = new Random();
+			for (int i = 0; i < longitud; i++)
+			{
+				for (int k = 0; k < largo; k++)
+				{
+					if (generador.Next(0, 2) == 1)
+						estrellas[i, k] = '*';
+					else
+						estrellas[i, k] = ' ';
+				}
+			}
+
+			for (int i = 0; i < longitud; i++)
+			{
+				for (int k = 0; k < largo; k++)
+				{
+					Console.Write(estrellas[i, k]);
+				}
+
+				Console.WriteLine();
+			}
+			Console.WriteLine("Pulsa una tecla ... ");			
+			Console.ReadKey();
+        }
+    }
+}
---
+++ visual_studio/practica/practica/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c = System.Console;
+
+namespace practica
+{
+    public class practica
+    {
+        public static void Main(string[] args)
+        {
+            double num1 = 0;
+            double num2 = 0;
+            char operacion;
+
+            string salir = "";
+            
+            while (!(salir == "fin"))
+            {
+                // Peticion de datos
+               // Console.WriteLine();
+                Console.WriteLine("Introduzca el numero 1");
+                salir = Console.ReadLine();
+                if (salir == "fin")
+                {
+                    num1 = double.Parse(salir);
+                }
+                Console.WriteLine("Introduzca la operacion");
+                string temp = Console.ReadLine(); // Control de input
+                operacion = temp[0];
+
+                if ((operacion != 'R') && (operacion != 'F') && (operacion != 'A'))
+                {
+                    Console.WriteLine("Introduzca el numero 2");
+                    num2 = double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine(); 
+                Console.WriteLine("Resultado:");
+                Console.WriteLine();                      
+                // calculo de operaciones
+
+                switch (operacion)
+                {
+                    case '+':
+                        c.WriteLine(num1 + num2);
+                        break;
+
+                    case '-':
+                        c.WriteLine(num1 - num2);
+                        break;
+
+                    case '*':
+                        c.WriteLine(num1 * num2);
+                        break;
+
+                    case '/':
+                        c.WriteLine(num1 / num2);
+                        break;
+
+                    case 'P':
+                        int i = 0;
+                        while(i<(int)num2-1)
+                        {
+                            num1 = num1*num1;
+                            i++;
+                        }
+                        c.WriteLine(num1);
+                        break;
+
+                    case 'R':
+                        num1 = Math.Sqrt(num1);
+                        c.WriteLine(num1);
+                        break;
+
+                    case 'A':
+                        if (num1 < 0)
+                        {
+                            num1 = num1 * (-1);
+                        }
+                        c.WriteLine(num1);
+                        break;
+
+                    case 'F':
+                        int temp3 = (int) num1;
+                        for (int k = (int)num1-1; k > 0; k--)
+                        {
+                            temp3 = temp3 * k;
+                        }
+                        c.WriteLine (temp3);
+                        break;
+
+                    default:
+                        break;
+
+                }
+                Console.WriteLine(); 
+            }    
+        }
+    }
+}

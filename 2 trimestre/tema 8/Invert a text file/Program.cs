979ea328651e2a4dc37fe22734119ff172fd90e0
v0lt3r <volter90@gmail.com>
2014-02-05 18:15:01 +0100

    nuevo

---
+++ 2 trimestre/tema 8/Invert a text file/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Invert_a_text_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader ficha;
+
+            ficha = File.OpenText("fichero.txt");
+            int contador = 0;
+            string linea;
+
+            do
+            {
+                linea = ficha.ReadLine();
+                contador++;
+            } while (linea != "");
+            contador--;
+            ficha.Close();
+
+            string [] array = new string [contador];
+            ficha = File.OpenText("fichero.txt");
+            for (int i = contador-1; i >= 0; i--)
+            {
+                array[i] = ficha.ReadLine();
+            }
+            ficha.Close();
+
+            StreamWriter fichero;
+            fichero = File.CreateText("fichero.tnv");
+
+            for (int i = 0; i < contador; i++)
+            {
+                fichero.WriteLine(array[i]);
+            }
+
+            fichero.Close();
+            
+        }
+    }
+}
---
+++ 2 trimestre/tema 8/More/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace More
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string nombre;
+            if (args.Length == 0)
+            {
+               Console.WriteLine("escriba el nombre deseado");
+               nombre = Console.ReadLine();
+            }
+            else if (args.Length == 1)
+            {
+                nombre = args[0];
+            }
+            else 
+            {
+                return -1;
+            }
+
+            StreamReader fichero;
+            string linea = " ";
+            fichero = File.OpenText(nombre);
+
+            while (linea != null)
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    linea = fichero.ReadLine();
+                    if (linea != null)
+                    {
+                        for (int k = 0; (k < 79 && k < linea.Length); k++)
+                        {
+                           Console.Write(linea[k]);
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        i = 25;
+                    }
+                }
+                if (linea != null)
+                {
+                    Console.WriteLine("pulsar una tecla ....");
+                    Console.ReadKey();
+                }
+            }
+            fichero.Close();
+
+            return 0;
+        }
+    }
+}

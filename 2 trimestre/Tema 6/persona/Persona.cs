7325ec41bc29610d33eeef10d4d799012027a42b
v0lt3r <volter90@gmail.com>
2014-02-05 16:45:16 +0100

    random

---
+++ 2 trimestre/Tema 6/persona/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace persona
+{
+    class Persona
+    {
+        string nombre;
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void Saludar()
+        {
+            Console.WriteLine("hola soy {0}", this.nombre);
+        }
+    }
+}

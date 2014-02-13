f5c1d76a08929da2e21804539c2070ae26c041b5
v0lt3r <volter90@gmail.com>
2014-02-13 16:50:50 +0100

    matriz

---
+++ 2 trimestre/Tema 7/matriz 7.6.1/Matriz.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz_7._6._1
+{
+    class Matriz
+    {
+        static int largo = 3;
+        static int ancho = 3;
+
+        int[,] matriz = new int [largo, ancho];
+
+        public Matriz()
+        {
+            for (int i = 0; i < largo; i++)
+            {
+                for (int j = 0; j < ancho; j++)
+                {
+                    matriz[i, j] = 0;
+                }
+            }
+        }
+
+        public void creaMatriz()
+        {
+            for (int i = 0; i < largo; i++)
+            {
+                for (int j = 0; j < ancho; j++)
+                {
+                    Console.WriteLine("escriba el valor del elemento en la linea {0} y en la columna {1}", i + 1, j + 1);
+                    while (!int.TryParse(Console.ReadLine(), out matriz[i, j])) 
+                    {
+                        Console.WriteLine("Error!");
+                        Console.WriteLine("escriba el valor del elemento en la linea {0} y en la columna {1}", i + 1, j + 1);
+                    } 
+                }
+            }
+        }
+
+        public void modificarValor()
+        {
+            int linea;
+            int columna;
+            int valor;
+
+            Console.WriteLine("escriba la linea del valor a modificar");
+            while (!int.TryParse(Console.ReadLine(), out linea) || linea > largo)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine("escriba la linea del valor a modificar");
+            }
+
+            Console.WriteLine("escriba la columna del valor a modificar");
+            while (!int.TryParse(Console.ReadLine(), out columna) || columna > ancho)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine("escriba la columna del valor a modificar");
+            }
+
+            Console.WriteLine("escriba el valor a modificar"); 
+            while (!int.TryParse(Console.ReadLine(), out valor)) 
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine("escriba el valor a modificar");
+            } 
+
+            matriz[(linea-1), (columna-1)] = valor;
+        }
+
+        public int leerposicion()
+        {
+            int linea;
+            int columna;
+
+            Console.WriteLine("escriba la linea del valor a leer");
+            while (!int.TryParse(Console.ReadLine(), out linea) || linea > largo)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine("escriba la linea del valor a leer");
+            }
+
+            Console.WriteLine("escriba la columna del valor a leer");
+            while (!int.TryParse(Console.ReadLine(), out columna) || columna > ancho)
+            {
+                Console.WriteLine("Error!");
+                Console.WriteLine("escriba la columna del valor a leer");
+            }
+
+            return matriz[linea-1, columna-1];
+        }
+
+        public void imprimirmatriz()
+        {
+            for (int i = 0; i < largo; i++)
+            {
+                for (int j = 0; j < ancho; j++)
+                {
+                    Console.Write("\t" + matriz[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Matriz operator +(Matriz mat1, Matriz mat2)
+        {
+            Matriz matriz3 = new Matriz();
+            for (int x = 0; x < largo; x++)
+			{
+			        for (int y = 0; y < ancho; y++)
+			        {
+                        matriz3.matriz[x, y] = mat1.matriz[x, y] + mat2.matriz[x, y];
+			        }
+			}
+            return matriz3;
+        }
+    }
+}
---
+++ 2 trimestre/Tema 7/matriz 7.6.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz_7._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Primera Matriz"); 
+            Matriz m1 = new Matriz();
+            m1.creaMatriz();
+
+            Console.WriteLine();
+            Console.WriteLine("Segunda Matriz");
+            Matriz m2 = new Matriz();
+            m2.creaMatriz();
+
+            Matriz m3 = m1 + m2;
+            Console.WriteLine();
+            Console.WriteLine("Suma de las dos matrices");
+            m3.imprimirmatriz();
+
+            Console.WriteLine();
+            m1.modificarValor();
+            m1.imprimirmatriz();
+
+            Console.WriteLine();
+            Console.WriteLine(m2.leerposicion());
+            m2.imprimirmatriz();
+            Console.WriteLine();
+
+        }
+    }
+}

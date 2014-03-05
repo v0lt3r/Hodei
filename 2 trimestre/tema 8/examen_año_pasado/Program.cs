8875b042a9f2a1a58fe8156842323748387c9c91
v0lt3r <volter90@gmail.com>
2014-03-05 01:37:05 +0100

    mas archivos

---
+++ 2 trimestre/tema 8/examen_año_pasado/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace examen_año_pasado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ej 1
+
+            StreamReader fichero;
+            string linea;
+            fichero = File.OpenText("prueba.txt");
+            do
+            {
+                linea = fichero.ReadLine();
+                if (linea != null)
+                {
+                    string[] array2 = linea.Split('(');
+
+                    string[] array = linea.Split('\'');
+
+                    StreamWriter fichero2;
+
+                }    
+            }
+            while (linea != null);
+            fichero.Close();
+        }
+    }
+}

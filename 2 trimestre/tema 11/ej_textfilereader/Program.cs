38215b5c923b53b04f2708b36580c1ed0efe7f51
v0lt3r <volter90@gmail.com>
2014-03-12 17:52:11 +0100

    simple textreader

---
+++ 2 trimestre/tema 11/ej_textfilereader/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace ej_textfilereader
+{
+	class Program
+	{
+		static ArrayList myList = new ArrayList();
+
+		static void Main(string[] args)
+		{
+			StreamReader fichero;
+			Console.WriteLine("que fichero deseea leer?");
+			string nombre_fichero = Console.ReadLine();
+			fichero = File.OpenText(nombre_fichero);
+			char tecla = ' ';
+
+			if (fichero != null)
+			{
+				do{
+
+				
+				
+
+
+				}while(tecla != (char)27);
+			}
+
+		}
+
+		static void ReadFile(StreamReader fichero)
+		{
+			string temp;
+			while ((temp = fichero.ReadLine()) != null)
+			{
+				myList.Add(temp);
+			}
+			return;
+		}
+
+		static void ShowInfo(int inicio)
+		{
+			Console.WriteLine(inicio + "-" + inicio+20 + "/" myList.Count);
+		}
+
+		static void DisplayForm(int inicio)
+		{
+
+		}
+
+	}
+}
